2cf53a49b1469cae104591d889e6d24685b79b67
h.nakano <h.nakano@variosecure.net>
2021-02-09 17:56:52 +0900

    複数クエリを実装

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
-var dbopt = {
+let dbopt = {
     user: "sdnspf",
     password: "$SdnsPf",
     host: "localhost",
@@ -8,13 +8,40 @@ var dbopt = {
 }
 const pg = new Client(dbopt);
 
-var network_id = 'aaa'
+let network_id = 'e9dfbcf5-553a-4866-b04a-ecba568c8f49';
 
 const aptask_query = "INSERT INTO vtm_approval_check_task (group_id) values ($1)";
 
 pg.connect()
-    .then(() => pg.query(aptask_query, [ network_id ]))
-    .then("select * from vtm_approval_check_task")
-    .then(results => console.table(results.rows))
-    .catch((e => console.log(e)))
-    .finally((() => pg.end()));
+    .catch(e => {
+        console.log(e);
+        pg.end();
+    });
+
+pg.query(aptask_query, [ network_id ])
+    .cache(e => console.log(e));
+
+const query = "SELECT DISTINCT c.vm_ip_address AS vm_ip_address " +
+    "FROM common_customer_master AS c, sdns_group_master AS g " +
+    "WHERE c.customer_id = g.customer_id AND g.group_id=$1";
+pg.query(query, [ network_id ])
+    .then(dbresult => {
+//		  console.log("DB result=", dbresult);
+        if (dbresult.rowCount <= 0) {
+            console.log("customer_id or vm_ip_address not found.")
+            return;
+        }
+        const vm_ip_address = dbresult.rows[0].vm_ip_address;
+        if (!vm_ip_address) {
+            console.log("customer_id or vm_ip_address not found.")
+            return;
+        }
+        console.log("Triggerring VM(" + vm_ip_address + ")'s API");
+    })
+    .catch(err => {
+        console.error('Query error.', err.stack);
+        pg.end();
+    })
+    .then(() => {
+        pg.end();
+    });
