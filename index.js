161cb57fab1850c2124abaed4534ea53232ccc84
h.nakano <h.nakano@variosecure.net>
2021-02-09 09:54:33 +0900

    とりあえず動くようにしてみた

--- index.js
+++ index.js
@@ -10,17 +10,11 @@ const pg = new Client(dbopt);
 
 var network_id = 'aaa'
 
-pg.connect()
-    .cache((e => console.log(e)));
-
 const aptask_query = "INSERT INTO vtm_approval_check_task (group_id) values ($1)";
-pg.query(aptask_query, [ network_id ])
+
+pg.connect()
+    .then(() => pg.query(aptask_query, [ network_id ]))
     .then("select * from vtm_approval_check_task")
     .then(results => console.table(results.rows))
-    .cache(err => {
-        console.log(err);
-        pg.end();
-    })
-    .then(() => {
-        pg.end();
-    });
+    .catch((e => console.log(e)))
+    .finally((() => pg.end()));
